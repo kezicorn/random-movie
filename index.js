6dd79b1c7935c9bcfeb2c9e863715bd4ec1a387f
Chris Clarke <chrisclarke1977@gmail.com>
2017-07-05 18:59:58 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,91 @@
+function randomItem (list) 
+{
+  return list[Math.floor(Math.random() * list.length)];
+};
+
+
+var ws = {};
+
+ws.data = { };
+
+document.getElementById("list").innerHTML.split(",").forEach(function(i){ ws.data[i] = {name:i, done: false }});
+
+function spinItem() {
+    document.getElementById("result").innerHTML = randomItem(Object.keys(ws.data));
+}
+
+
+var oEl = document.getElementById("container");;
+
+function renderTask(id, task){
+  var o = '';
+
+  o+= '<tr data="'+id+'"><td class="js-edit">Movie: </td><td>'+task.name+'</td><td>';
+  if(task.done){
+    o+='Watched';
+  } else {
+    o+= 'To Watch';
+  }
+  o+='</td></tr>';
+
+  return o;
+}
+
+function renderList(tasks){
+  var o = '',header = '', footer = '';
+
+  header = '<button>Add</button><input id="newTask" type="text"/><table><thead><tr><th></th><th>Movie</th><th>Done</th></tr></thead><tbody>';
+  footer = '</tbody></table>';
+
+  for (var v in tasks) {
+    o += renderTask(v, tasks[v]);
+  }
+
+  return header + o + footer;
+}
+
+function editTask(el){
+  /* get el replace with row inc input for editor */
+  var o = '', id = el.attributes.data.value;
+  o +='<tr data="'+id+'"><td>Save</td><td></td><td><input type="option" checked="';
+  if(ws.data[id].done){ o+='checked'; }
+  o+= '"></td></tr>';
+  return o;
+}
+
+function getList() {
+  oEl.innerHTML = renderList(ws.data);
+  var p = document.getElementsByTagName("tr");
+  var zen = 0;
+  for (var b in p) { 
+    if(parseInt(b) == b) {
+      p[b].addEventListener("click",function(e){
+        var o = e.currentTarget, id = e.currentTarget.attributes.data.value;
+    
+        ws.data[id].done = !ws.data[id].done;
+        getList();
+      }); 
+      zen++;
+    }
+  }
+
+  document.getElementById("newTask").addEventListener("change",function(e){
+    ws.data["task-"+zen] = {"name":document.getElementById("newTask").value,done: false};
+    getList();
+  });
+  if(document.getElementById("editTask") == null) {
+  document.getElementById("newTask").focus(); } else {
+  document.getElementById("editTask").focus(); 
+  }
+}
+function bindAction(el,a) {
+  if(el.hasOwnProperty() == false) {  
+
+    el.addEventListener(a,function(e){
+      console.log(e);
+    });
+  }
+}
+
+getList();
+document.getElementById("newTask").focus();
